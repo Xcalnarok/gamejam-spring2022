607d7d76e7562c064f80ee3720bb16c3aadd022f
Xcalnarok <xcalnarok@gmail.com>
2022-04-23 17:36:48 -0700

    Yarn Test & Audio Attempt

--- SpringGameJam2022/Assets/Scripts/DontDestroy.cs
+++ SpringGameJam2022/Assets/Scripts/DontDestroy.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
-    private void Awake()
-    {
-        DontDestroyOnLoad(this.gameObject);
-    }
+	public static DontDestroy Instance;
+	private void Awake()
+	{
+		// If there is not already an instance of SoundManager, set it to this.
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		//If an instance already exists, destroy whatever this object is to enforce the singleton.
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+
+		//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+		DontDestroyOnLoad(gameObject);
+	}
 }
